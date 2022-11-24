586cbce2cf7cd18f60991d67e2cb83cd44025d72
Mateus Nogueira Leal <106922117+NOGMateus@users.noreply.github.com>
2022-11-24 19:49:56 -0300

    Upload File
    All folder of the project

---
+++ Hotel/Hotel/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Hotel // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- SISTEMA DE HOSPEDAGEM -----");
+            Console.WriteLine("---------------------------------");
+
+            //Array de Quartos
+
+
+
+            Console.WriteLine("Quantos quartos vão ser alugados ? (1-10) ");
+            int num = Int32.Parse(Console.ReadLine());
+
+            //tamanho de quarto fixo
+            Quarto[] Quarto = new Quarto[10];
+
+
+
+
+            for (int i = 0 ; i < num; i++)
+            {
+                Console.WriteLine("--- CADASTRO DE QUARTOS ---");
+                Console.WriteLine(" - Dados de Aluguel -  ");
+                Console.WriteLine("Digite seu nome: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Digite seu e-mail: ");
+                string mail = Console.ReadLine();
+
+                Console.WriteLine("Digite o número do quarto que você deseja alugar: ");
+                int numQuarto = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("----------------------------||-------------------------------");
+
+                Quarto[numQuarto] = new Quarto(name, mail, numQuarto);
+                Console.Clear();
+            }
+
+
+            Console.WriteLine("----- RElATÓRIO DE OCUPAÇÃO -----");
+
+            for (int i = 0; i < Quarto.Length; i++)
+            {
+                if (Quarto[i] != null)
+                {
+                    Console.WriteLine("Quarto # " + Quarto[i].Numeracao);
+                    Console.WriteLine("Nome do ocupante : " + Quarto[i].NomeAluguel);
+                    Console.WriteLine("E-mail do ocupante: " + Quarto[i].Email);
+                    Console.WriteLine("---------------------------------------------");
+
+                }
+
+            }
+
+            //TRATAMENTO DE ERROS
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Hotel/Hotel/Quarto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    internal class Quarto
+    {
+        public string NomeAluguel { get; set; }
+
+        public string Email { get; set; }
+        public int Numeracao { get; set; }
+
+
+
+       //Construtor
+       public Quarto(string nome, string email, int numeracao)
+        {
+            this.NomeAluguel = nome;
+            this.Email = email;
+            this.Numeracao = numeracao;
+
+        }
+
+
+
+
+        
+
+    }
+}
